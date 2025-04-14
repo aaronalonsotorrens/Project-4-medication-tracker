7d5179e46d37c63a42181a33fb29d90dd129eb1f
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-04-14 22:36:08 +0100

    Update deployment on heroku

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_5kNSoUap4XRf@ep-autumn-heart-a2y8633n.eu-central-1.aws.neon.tech/stain_kitty_shade_875416")
+os.environ.setdefault("SECRET_KEY", "abc123")
