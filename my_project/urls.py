406679ed2984ef90172a33617a28205b5d680b6d
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-04-26 19:06:57 +0100

    Create html file in project directory, add urls file to project and edit views accordingly to fit new edits

--- my_project/urls.py
+++ my_project/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from project_4_medication_tracker import views as project_4_medication_tracker_views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('project_4_medication_tracker/', project_4_medication_tracker_views.index, name="project_4_medication_tracker_index"),
+    path("", include('project_4_medication_tracker.urls'), name="project_4_medication_tracker-urls"),
 ]
