b073ff7f3778e839ba300137e9b4c7575faf3ecd
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-04-26 19:06:57 +0100

    Create html file in project directory, add urls file to project and edit views accordingly to fit new edits

--- project_4_medication_tracker/views.py
+++ project_4_medication_tracker/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Medication
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello World")
+class MedicationList (generic.ListView):
+    model = Medication
+    template_name = "medication_tracker.html"
