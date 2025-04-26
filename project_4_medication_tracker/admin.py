1fbb13de96c7fa322940bccc1fcbdd3337080b64
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-04-26 17:58:48 +0100

    Add a filter system tothe medication  class in admin py

--- project_4_medication_tracker/admin.py
+++ project_4_medication_tracker/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Medication
 
 # Register your models here.
-admin.site.register(Medication)
+@admin.register(Medication)
+class MedicationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'dosage', 'frequency_per_day', 'start_date', 'end_date', 'created_on')
+    list_filter = ('start_date', 'end_date')
+    ordering = ('-created_on', 'user')
+    search_fields = ('name', 'user__username')
+
+    def frequency_per_day(self, obj):
+        return f"{obj.frequency} times/day"
+    frequency_per_day.short_description = 'Frequency per Day'
