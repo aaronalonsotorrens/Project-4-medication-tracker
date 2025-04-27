3fa0a9d556589901f0e3b68fbff10def61950e7c
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-04-27 21:29:36 +0100

    Edit URL of the app to include side effects

--- medication_tracker/urls.py
+++ medication_tracker/urls.py
@@ -1,6 +1,8 @@
-from . import views 
 from django.urls import path
+from . import views
 
 urlpatterns = [
-path ('', views .MedicationList.as_view(), name='home'), 
+    path('', views.HomePageView, name='home'),
+    path('medications/', views.MedicationList.as_view(), name='medication_list'),
+    path('medications/<int:medication_id>/add-side-effect/', views.add_side_effect, name='add_side_effect'),
 ]
