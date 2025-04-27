80eda13f06dd314f32d9ad0640e9bd92accced46
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-04-27 14:46:15 +0100

    Delete migrations file due to edit to the app name. Create index and base html template

--- medication_tracker/urls.py
+++ medication_tracker/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-path ('', views .MedicationList.as_view(), name=' home'), 
+path ('', views .MedicationList.as_view(), name='home'), 
 ]
