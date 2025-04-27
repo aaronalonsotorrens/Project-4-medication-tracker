08cc7babcd3e8757f89fda6196ec52f52bf33a0c
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-04-27 19:38:33 +0100

    Edit views to include authentification, make distinction between admin and user, start to craft side effects for medication as a comment

--- medication_tracker/views.py
+++ medication_tracker/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
-from .models import Medication
+from .models import Medication, SideEffect
+from .forms import SideEffectForm
 
-# Create your views here.
+def HomePageView(request):
+    return render(request, 'home.html')
 
 class MedicationList(generic.ListView):
-    queryset = Medication.objects.all()
-    template_name = "medication_tracker/index.html"
+    model = Medication
+    template_name = 'medication_list.html'
+    context_object_name = 'medications'
     paginate_by = 6
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Medication.objects.all()
+        return Medication.objects.filter(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['side_effect_form'] = SideEffectForm()
+        return context
+
+def add_side_effect(request, medication_id):
+    if request.method == "POST":
+        medication = Medication.objects.get(id=medication_id)
+        form = SideEffectForm(request.POST)
+        if form.is_valid():
+            side_effect = form.save(commit=False)
+            side_effect.user = request.user
+            side_effect.medication = medication
+            side_effect.save()
+    return redirect('medication_list')
