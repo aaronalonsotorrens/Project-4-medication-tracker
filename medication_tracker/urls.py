d401845607b705ab7ce9dbbc4310dd1a1dfa890d
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-05-05 20:38:10 +0100

    Create html form for create, update and delete options. Add class functions to the views py for each of these options

--- medication_tracker/urls.py
+++ medication_tracker/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView, name='home'),
     path('medications/', views.MedicationList.as_view(), name='medication_list'),
+    path('medications/add/', views.MedicationCreate.as_view(), name='medication_add'),
+    path('medications/<int:pk>/edit/', views.MedicationUpdate.as_view(), name='medication_edit'),
+    path('medications/<int:pk>/delete/', views.MedicationDelete.as_view(), name='medication_delete'),
     path('medications/<int:medication_id>/add-side-effect/', views.add_side_effect, name='add_side_effect'),
 ]
