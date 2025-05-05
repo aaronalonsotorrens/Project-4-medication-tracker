da09a88e9de51ceae32f25ba9c3251191bb5aa25
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-05-05 20:38:10 +0100

    Create html form for create, update and delete options. Add class functions to the views py for each of these options

--- medication_tracker/views.py
+++ medication_tracker/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 from .models import Medication, SideEffect
-from .forms import SideEffectForm
+from .forms import MedicationForm, SideEffectForm
 
 def HomePageView(request):
     return render(request, 'medication_tracker/index.html')
@@ -32,3 +35,35 @@ def add_side_effect(request, medication_id):
             side_effect.medication = medication
             side_effect.save()
     return redirect('medication_list')
+
+
+class MedicationCreate(LoginRequiredMixin, CreateView):
+    model = Medication
+    form_class = MedicationForm
+    template_name = 'medication_tracker/medication_form.html'
+    success_url = reverse_lazy('medication_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+
+class MedicationUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Medication
+    form_class = MedicationForm
+    template_name = 'medication_tracker/medication_form.html'
+    success_url = reverse_lazy('medication_list')
+
+    def test_func(self):
+        medication = self.get_object()
+        return self.request.user == medication.user or self.request.user.is_superuser
+    
+
+class MedicationDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Medication
+    template_name = 'medication_tracker/medication_confirm_delete.html'
+    success_url = reverse_lazy('medication_list')
+
+    def test_func(self):
+        medication = self.get_object()
+        return self.request.user == medication.user or self.request.user.is_superuser
