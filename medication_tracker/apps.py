c949e1f73ca722f49293b1f32aa2f6a80dec3d98
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-05-11 19:49:14 +0100

    Add additional fields to the signup to be able to allow meaningful data. Add code to the forms and and models py, create file for signals py and update the app py

--- medication_tracker/apps.py
+++ medication_tracker/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class Project4MedicationTrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'medication_tracker'
+
+
+    def ready(self):
+        import medication_tracker.signals
+
