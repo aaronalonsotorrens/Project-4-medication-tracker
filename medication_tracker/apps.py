8f624c78f4ec054736c2a27144d23cdc1cd8634f
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-05-13 16:20:07 +0100

    Correct user register and login. Remove the signals.py and edit the apps.py. Remove integers from base html to allow template to run without issues

--- medication_tracker/apps.py
+++ medication_tracker/apps.py
@@ -1,11 +1,5 @@
 from django.apps import AppConfig
 
-
 class Project4MedicationTrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'medication_tracker'
-
-
-    def ready(self):
-        import medication_tracker.signals
-
