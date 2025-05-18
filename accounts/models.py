96d506164fdbae580d1fd0dd28697f2060801c50
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-05-18 15:21:47 +0100

    Add country fields package and incorpoaret to accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
 # Create your models here.
 
@@ -14,7 +15,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     age = models.PositiveIntegerField()
-    country = models.CharField(max_length=100)
+    country = CountryField(blank_label="(select country)")
 
     def __str__(self):
         return f"{self.user.username}'s profile"
