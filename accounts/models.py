29bec7ee5bb187f034ce371a93a79b204361bfcd
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-05-18 22:24:22 +0100

    Edit the account models.py to match the dummy data and clear error related to character field

--- accounts/models.py
+++ accounts/models.py
@@ -7,13 +7,13 @@ from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     GENDER_CHOICES = [
-        ('M', 'Male'),
-        ('F', 'Female'),
-        ('O', 'Other'),
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
     ]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
     age = models.PositiveIntegerField()
     country = CountryField(blank_label="(select country)")
 
