831078e824be1f80ccee29658b07c6c7db1ab92a
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-05-29 21:18:20 +0100

    Add deployment and acknowledgments to Readme

--- env.py
+++ env.py
@@ -1,7 +1,9 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_5kNSoUap4XRf@ep-autumn-heart-a2y8633n.eu-central-1.aws.neon.tech/stain_kitty_shade_875416")
+    "DATABASE_URL", "postgresql://neondb_owner:npg_5kNSoUap4XRf@ep-autumn-heart-a2y8633n.eu-central-1.aws.neon.tech/stain_kitty_shade_875416"
+)
 os.environ.setdefault("SECRET_KEY", "abc123")
 os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary://155677233578582:a2HDhnwqGoQhlqhAkESt8omkUzQ@doleufxil")
+    "CLOUDINARY_URL", "cloudinary://155677233578582:a2HDhnwqGoQhlqhAkESt8omkUzQ@doleufxil"
+)
