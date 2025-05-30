52a2067c0096c95e7ad202b595ac64b6bd1e5e77
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-05-30 10:19:40 +0100

    Deploy test

--- medication_tracker/settings.py
+++ medication_tracker/settings.py
@@ -44,7 +44,7 @@ else:
 # Security
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = ['project-4-medication-tracker.herokuapp.com', '127.0.0.1', 'localhost',]
 
 # Installed apps
 INSTALLED_APPS = [
